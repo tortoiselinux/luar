cad68d1f20bcf1bf386158478898efb5f66d8169
wellyton-xs <wellyton.offcer@gmail.com>
2025-05-01 21:38:59 -0300

    Fix: unfinished string near '"' when packing
    This commit have a lot of changes in both (packer and linker)
    
    The code was refactored, making it easier to read and maintain.
    
    In addition, both the linker and the packer were updated to prevent
    bad code generation when creating embedded scripts.
    
    When the code was packaged with the packer, the mark was generated
    together with important opcodes in the binary file, corrupting it
    and preventing its full functioning.
    
    During the linking stage, sanitization was added to the shebangs,
    automatically removing them.
    
    A summary of the changes:
    
    - mark added automatically by the packer
    - sanitization of shebangs by the linker
    - linked and packaged files are now generated with namespace to
      avoid accidental overwriting

--- linker.lua
+++ linker.lua
@@ -1,11 +1,9 @@
 #!/usr/bin/env lua
 
 local t = require("tlib")
-local l = require("linkerlib")
 
-function help()
-   local helpmsg=
-[[
+local function help()
+   local helpmsg = [[
 
  🐢 Tortoise Lua Linker
 
@@ -26,44 +24,78 @@ function help()
  see: https://github.com/tortoiselinux/packer
  
 ]]
-print(helpmsg)	
+print(helpmsg)
 end
-local MAGIC_MARK = "--[[LUA_SCRIPT_START]]"
+
+local function exit(code)
+	os.exit(code)
+end
+
+local MAGIC_MARK = "-- LUA_SCRIPT_START"
 local linked_script = MAGIC_MARK .. "\n"
+local filename, script
+local namespace = "linked_"
+
+local function extract_shebang(content)
+   local first_line = content:match("^(#![^\n]*)\n")
+   if first_line then
+      content = content:gsub("^#![^\n]*\n", "")
+      return first_line, content
+   end
+   return nil, content
+end
+
+local function link(lua_module)
+	local content = "\n" .. "--modid:" .. lua_module .. "\n"
+	local mod = t.read_file(lua_module)
+	mod = mod:gsub("^#![^\n]*\n", "")
+   	mod = mod:gsub("[\r\n]*%s*return%s+[%w_]+%s*$", "")
+   	content = content .. mod .. "\n--modend:" .. lua_module .. "\n"
+   	return content
+end
 
 -- filename = filename:match("^(.*)%.lua$")
-if t.verify_args(arg, {'h', '-h', 'help', '--help'}) then
+if #arg == 0 then
    help()
-else
-   if t.verify_args(arg, {'o', '-o', 'output', '--output'}) then
-      local filename = arg[2]
-      local script = arg[3]
-       
-      for i in ipairs(arg) do
-	 if i > 3 then
-	    print("linking the module [" .. i .. "]: " .. arg[i])
-	    linked_script = linked_script .. l.link(script, arg[i])
-	 end
-      end
-      local main_script = t.read_file(script)
-      main_script = main_script:gsub('local%s+(%w+)%s*=%s*require%s*%("([^"]+)"%)', "local %1 = %2")
-      linked_script = linked_script .. "\n" .. main_script
-      t.write_file(filename, "w", linked_script)
-
-      print("Generated linked file: ".. filename)
-   else
-      script = arg[1]
-      filename = script:match("([^/\\]+)%.lua$")
-      for i in ipairs(arg) do
-	 if i > 1 then
-	    print("linking the module [" .. i .. "]: " .. arg[i])
-	    linked_script = linked_script .. l.link(script, arg[i])
-	 end
-      end
-      local main_script = t.read_file(script)
-      main_script = main_script:gsub('local%s+(%w+)%s*=%s*require%s*%("([^"]+)"%)', "local %1 = %2")
-      linked_script = linked_script .. "\n" .. main_script
-      t.write_file(filename, "w", linked_script)
-      print("Generated linked file: " .. filename)
-   end
+   exit(1)
 end
+if t.verify_args(arg, { "h", "-h", "help", "--help" }) then
+	help()
+	exit(0)
+end
+if t.verify_args(arg, { "o", "-o", "output", "--output" }) then
+	filename = namespace .. arg[2]
+	script = arg[3]
+	for i in ipairs(arg) do
+		if i > 3 then
+			print("linking the module [" .. i .. "]: " .. arg[i])
+			linked_script = linked_script .. link(arg[i])
+		end
+	end
+	local main = t.read_file(script)
+	main = main
+		:gsub('local%s+(%w+)%s*=%s*require%s*%("([^"]+)"%)', "local %1 = %2")
+		:gsub("^#![^\n]*\n", "")
+	linked_script = linked_script .. "\n" .. main
+	t.write_file(filename, "w", linked_script)
+	print("Generated linked file: " .. filename)
+	exit(0)
+end
+
+script = arg[1]
+filename = namespace .. script:match("([^/\\]+)%.lua$")
+for i in ipairs(arg) do
+	if i > 1 then
+		print("linking the module [" .. i .. "]: " .. arg[i])
+		linked_script = linked_script .. link(arg[i])
+	end
+end
+local main = t.read_file(script)
+main = main
+	:gsub('local%s+(%w+)%s*=%s*require%s*%("([^"]+)"%)', "local %1 = %2")
+	:gsub("^#![^\n]*\n", "")
+
+linked_script = linked_script .. "\n" .. main
+t.write_file(filename, "w", linked_script)
+print("Generated linked file: " .. filename)
+exit(0)
--- packer.lua
+++ packer.lua
@@ -1,15 +1,9 @@
 #!/usr/bin/env lua
---[[LUA_SCRIPT_START]]
 
 local t = require("tlib")
 -- local luar = "build/runtime/luar"
-local luardir = "/usr/local/share/packer"
-local luar_unix = luardir .. "/luar"
-local luar_win = luardir .. "/luar.exe"
-local filename
-function help()
-   local helpmsg=
-[[
+local function help()
+   local help_msg = [[
 
  🐢 Tortoise Lua Packer
 
@@ -34,14 +28,26 @@ function help()
  To see how you can make embedded scripts
  see: https://github.com/tortoiselinux/packer 
 ]]
-print(helpmsg)
+print(help_msg)
 end
+
+local function exit(code)
+	os.exit(code)
+end
+
+local luardir = "/usr/local/share/packer"
+local luar_unix = luardir .. "/luar"
+local luar_win = luardir .. "/luar.exe"
+local script
+local filename
+local namespace = "packed_"
 
 local function concat(bin_path, script)
    local bin = assert(io.open(bin_path, "ab"))
    local txt = assert(io.open(script, "rb"))
    print("Appending: " .. script .. " to the binary enviroment: " .. bin_path)
    local content = txt:read("*a")
+   bin:write("\n--[[LUA_SCRIPT_START]]\n")
    bin:write(content)
    txt:close()
    bin:close()
@@ -53,62 +59,82 @@ local function copy_stub(src, dest)
    output:write(input:read("*a"))
    input:close()
    output:close()
-   if package.config:sub(1,1) == '/' then
+   if package.config:sub(1, 1) == "/" then
       os.execute("chmod +x " .. dest)
    end
 end
 
-function pack_to_linux(script, filename)
+local function pack_to_linux(script, filename)
    filename = filename or script
-   output_linux = "./" .. script:gsub("%.lua$", "")
+   local output_linux = "./" .. script:gsub("%.lua$", "")
    copy_stub(luar_unix, output_linux)
    concat(output_linux, script)
-   print("✅ Executável Linux criado: " .. filename:gsub("%.lua$", ""))
+   print("✅ Executável Linux criado: " .. namespace .. filename:gsub("%.lua$", ""))
    return output_linux
 end
 
-function pack_to_windows(script, filename)
+local function pack_to_windows(script, filename)
    filename = filename or script
-   output_windows = "./" .. script:gsub("%.lua$", "") .. ".exe"
+   local output_windows = "./" .. script:gsub("%.lua$", "") .. ".exe"
    copy_stub(luar_win, output_windows)
    concat(output_windows, script)
-   print("✅ Executável Windows criado: " .. filename:gsub("%.lua$", "") .. ".exe")
+   print("✅ Executável Windows criado: " .. namespace .. filename:gsub("%.lua$", "") .. ".exe")
    return output_windows
 end
 
-if t.verify_args(arg, {'h', '-h', 'help', '--help'}) then
+
+if #arg == 0 then
    help()
-elseif t.verify_args(arg, {"linux"}) then
-   if t.verify_args(arg,  {'o', '-o', 'output', '--output'}) then
-      filename = arg[3]
+   exit(1)
+end
+
+if t.verify_args(arg, { "h", "-h", "help", "--help" }) then
+   help()
+   exit(0)
+end
+
+if t.verify_args(arg, { "linux" }) then
+   if t.verify_args(arg, { "o", "-o", "output", "--output" }) then
+      filename = namespace .. arg[3]
       script = arg[4]
-      packed_script = pack_to_linux(script, filename)
-      os.rename(packed_script, filename)
+      local packed_script = pack_to_linux(script, filename)
+      os.rename(packed_script, filename:gsub("%.lua$", ""))
+      exit(0)
    else
       script = arg[2]
-      pack_to_linux(script)
+      local packed_script = pack_to_linux(script, namespace .. script)
+      os.rename(packed_script, script:gsub("%.lua$", ""))
+      exit(0)
    end
-elseif t.verify_args(arg, {"windows"}) then
-   if t.verify_args(arg,  {'o', '-o', 'output', '--output'}) then
+end
+
+if t.verify_args(arg, { "windows" }) then
+   if t.verify_args(arg, { "o", "-o", "output", "--output" }) then
       filename = arg[3] .. ".exe"
       script = arg[4]
-      packed_script = pack_to_windows(script, filename)
-      os.rename(packed_script, filename)
+      local packed_script = pack_to_windows(script, filename)
+      os.rename(packed_script, filename:gsub("%.lua$", ""))
+      exit(0)
    else
-      script = arg[2]
-      pack_to_windows(script)
+      script = namespace .. arg[2]
+      local packed_script = pack_to_windows(script, namespace .. script)
+      os.rename(packed_script, script:gsub("%.lua$", "") .. ".exe")
+      exit(0)
    end
+end
+
+if t.verify_args(arg, { "o", "-o", "output", "--output" }) then
+   filename = namespace .. arg[2]
+   script = arg[3]
+   local packed_script_linux = pack_to_linux(script)
+   local packed_script_windows = pack_to_windows(script)
+   os.rename(packed_script_linux, filename:gsub("%.lua$", ""))
+   os.rename(packed_script_windows, filename:gsub("%.lua$", "") .. ".exe")
 else
-   if t.verify_args(arg,  {'o', '-o', 'output', '--output'}) then
-      filename = arg[2]
-      script = arg[3]
-      packed_script_linux = pack_to_linux(script)
-      packed_script_windows = pack_to_windows(script)
-      os.rename(packed_script_linux, filename)
-      os.rename(packed_script_windows, filename .. ".exe")
-   else
-      script = arg[1]
-      pack_to_linux(script)
-      pack_to_windows(script)
-   end
+   script = arg[1]
+   filename = namespace .. script:match("([^/\\]+)%.lua$")
+   local packed_script_linux = pack_to_linux(script)
+   local packed_script_windows = pack_to_windows(script)
+   os.rename(packed_script_linux, filename)
+   os.rename(packed_script_windows, filename .. ".exe")
 end
